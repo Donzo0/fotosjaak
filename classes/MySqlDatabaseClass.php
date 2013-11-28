ef7a61b739b351488f229fa5c9afc66224948c66
Jason Binneveld <j.binneveld@live.nl>
2013-11-28 15:59:29 +0100

    2 mappen aangemaakt config.php en MySqlDatabaseClass aangemaakt

---
+++ classes/MySqlDatabaseClass.php
@@ -0,0 +1,13 @@
+<?php
+	require_once('config/config.php');
+
+	//class definitie van Deze class
+	class MySqlDatabaseClass 
+	{
+		//Constructor in php heeft altijd dezelfde naam.
+		public function __construct()
+		{
+			
+		}
+	}
+	
---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+	define('SERVERNAME', 'localhost');
+	define('USERNAME', 'root');
+	define('PASSWORD', '');
+	define('DATABASE', 'fotosjaak');
+	date_default_timezone_set("Europe/Amsterdam");
+	
+	
+	
