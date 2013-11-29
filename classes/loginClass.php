def6aebecf1dd73952b8fdb499677025ebfcb91a
Jason Binneveld <j.binneveld@live.nl>
2013-11-29 12:44:14 +0100

    mysqldatabaseclass verder gemaakt

--- classes/loginClass.php
+++ classes/loginClass.php
@@ -19,9 +19,25 @@
 		}
 		
 		// method find by_sql
-		public function find_by_sql($query)
+		public function find_by_sql($sql)
 		{
+			//haalt een variable uit de mysqldatabaseclass
 			global $database;
+			//haalt the method fire_query op
+			$result = $database->fire_query($sql);
+			//dit is een array. dit gaat login objecten bevatten
+			$object_array = array();
+			
+			// met deze while-lus vullen we het object-array met loginclass
+			while ($row = mysql_fetch_array($result)) 
+			{
+				$object = LoginClass();
+				$this->login_id = $row['login_id'];
+				$this->email 	= $row['emial'];
+				$this->password = $row['password'];
+				$this->isactivated = $row['isactivated'];
+				$this->registerdate = $row['registerdate'];
+			}
 		}
 		
 	}
