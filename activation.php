0bc61a39935775e1643c0cff425fe327564de789
Jason Binneveld <j.binneveld@live.nl>
2014-01-30 10:13:35 +0100

    UP to date gemaakt

--- activation.php
+++ activation.php
@@ -1,25 +1,76 @@
 <?php
-
-if (LoginClass::check_if_email_password_exists($_GET['email'],
-												$_GET['password']));
-{
-
-
+ require_once("class/LoginClass.php");
+ 
+ if ( isset($_POST['submit']))
+ {
+ 	//Controleer of de beide wachtwoorden overeenkome
+ 	
+ 	//Als dat zo is dan moet het wachtwoord worden geupdate
+ 	
+ 	//doorsturen naar de login_formpagina
+	if ( !strcmp($_POST['password'], $_POST['password_check']))
+	{
+		echo "De wachtwoorden zijn gelijk";
+		LoginClass::update_password_in_login($_POST['email'], $_POST['password']);
+		echo "Uw wachtwoord is succesvol gewijzigd. U wordt doorgestuurd<br>
+			  naar de inlogpagina";
+		header('refresh:5;url=index.php?content=login');
+	}
+	else
+    {
+		echo "De ingevulde wachtwoorden komen niet overeen. Probeer het<br>
+			  opnieuw. U wordt teruggestuurd";
+		header('refresh:5;url=index.php?content=activation&email='.$_POST['email'].'&password='.$_POST['old_password']);
+	}
+	
+ }
+ else 
+ {
+ 	
+ if (LoginClass::check_if_email_password_exists($_GET['email'],
+ 												$_GET['password']))
+ {
 ?>
-	<p>Welkom op de site uw acount wordt geactiveerd nadat u een nieuwe wachtwoord heeft ingevuld</p>
-	<form>
-		<fieldset>
-			<label for="w81"></label>
-				<input type="password" name="password" id="w81" placeholder="Nieuw Wachtwoord">
-			<label for="w82"></label>
-				<input type="password" name="password" id="w82" placeholder="Herhaal Wachtwoord">
-		</fieldset>
+	<p>Welkom op de site uw account wordt geactiveerd nadat<br>
+	   U een nieuw wachtwoord heeft gekozen.
+	</p>
+	<br>  
+	<form action='index.php?content=activation' method='post'>
+		<table>
+			<tr>
+				<td>nieuw wachtwoord (12 tekens)</td>
+				<td>
+					<input type='password'
+						   name='password'
+						   size='12'
+						   maxlength='12' />
+				</td>
+			</tr>
+			<tr>
+				<td>nieuw wachtwoord (check)</td>
+				<td><input type='password'
+						   name='password_check'
+						   size='12'
+						   maxlength='12' /></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><input type='submit' 
+						   name='submit'
+						   value='verstuur' />
+					<input type='hidden' value='<?php echo $_GET['email']; ?>' name='email' />				<input type='hidden' value='<?php echo $_GET['password']; ?>' name='old_password' /></td>
+					
+			</tr>
+		</table>
 	</form>
 
 <?php
-}
-else
-{
-	echo "u Heeft geen rechten om op deze pagina kan komen";
-	header("refresh:5;index.php?content=homepage");
-}
+ }
+ else 
+ {
+    echo "U heeft geen rechten op deze pagina. U wordt<br> 
+    	  doorgestuurd naar de homepage";
+    header("refresh:5;index.php?content=homepage");
+ }
+ }
+?>
